44aef93d7d8d19fd7425833cfc006c352f52cb52
1967cooder <silvanalindholm@hotmail.com>
2026-03-12 13:26:20 +0200

    07-ai-notes muistiinpanot tiedostosta db.js näkyvät nyt selaimella.

--- 02-ai-full-stack-open/osa2/07-ai-notes/src/main.jsx
+++ 02-ai-full-stack-open/osa2/07-ai-notes/src/main.jsx
@@ -1,11 +1,4 @@
 import ReactDom from "react-dom/client";
 import App from "./App.jsx";
 
-import axios from "axios";
-
-axios.get("http://localhost:3001/notes").then((response) => {
-  const notes = response.data;
-  console.log(notes);
-});
-
 ReactDom.createRoot(document.getElementById("root")).render(<App />);
