51c5efacec3405ede8bcae379a45f4ce7f15fcef
1967cooder <silvanalindholm@hotmail.com>
2026-03-12 14:38:17 +0200

    07-ai-notes uuden muistiinpanon lisääminen näkyy näytöllä. Voidaan suodattaa vain terkeät muistiinpanot näkyviin.

--- 02-ai-full-stack-open/osa2/07-ai-notes/src/App.jsx
+++ 02-ai-full-stack-open/osa2/07-ai-notes/src/App.jsx
@@ -18,14 +18,46 @@ const App = () => {
   }, []);
   console.log("render", notes.length, "notes");
 
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      id: String(notes.length + 1),
+      content: newNote,
+      important: Math.random() > 0.5,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log("input-kentän arvo: ", event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
     <>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      <section>
+        <h1>Notes</h1>
+        <p>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? "important" : "all"}
+          </button>
+        </p>
+        <ul>
+          {notesToShow.map((note) => (
+            <Note key={note.id} note={note} />
+          ))}
+        </ul>
+      </section>
+      <section className="addnote">
+        <h2>Add a new note</h2>
+        <form onSubmit={addNote}>
+          <input value={newNote} onChange={handleNoteChange} />
+          <button type="submit">save</button>
+        </form>
+      </section>
     </>
   );
 };
