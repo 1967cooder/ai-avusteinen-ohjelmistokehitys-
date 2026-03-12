3244c0871d67ac2a19a11b3b1339d69c717aaa2a
1967cooder <silvanalindholm@hotmail.com>
2026-03-12 15:06:21 +0200

    07-ai-notes uusi muistiinpano päivittää tiedostoa db.json.

--- 02-ai-full-stack-open/osa2/07-ai-notes/src/App.jsx
+++ 02-ai-full-stack-open/osa2/07-ai-notes/src/App.jsx
@@ -10,27 +10,25 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
     axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
       setNotes(response.data);
     });
   }, []);
-  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
-      id: String(notes.length + 1),
       content: newNote,
       important: Math.random() > 0.5,
     };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
-    console.log("input-kentän arvo: ", event.target.value);
     setNewNote(event.target.value);
   };
 
